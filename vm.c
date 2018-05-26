a394fb24ff3cbe584910088a544e695ad1e4b642
cameronmorin <cmori007@ucr.edu>
2018-05-25 19:01:49 -0700

    Changed the implementation of page copying due to new location

--- vm.c
+++ vm.c
@@ -177,7 +177,7 @@ switchuvm(struct proc *p)
   popcli();
 }
 
-// Load the initcode into address 0 of pgdir.
+// Load the initcode into address 0 of pgdir.                 --------------------->Change to USERSTACK???
 // sz must be less than a page.
 void
 inituvm(pde_t *pgdir, char *init, uint sz)
@@ -335,6 +335,22 @@ copyuvm(pde_t *pgdir, uint sz)
     if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
       goto bad;
   }
+
+  
+  uint stackSize = 1;                                                                       //ADDED LAB3
+  i = USERSTACKBASE - PGESIZE + 1;
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
+      goto bad;
+
   return d;
 
 bad:

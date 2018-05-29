9ce402d439e7568ff1042161820c8cb550cc5266
cameronmorin <cmori007@ucr.edu>
2018-05-28 18:50:33 -0700

    Change kstack copy in fork()

--- proc.c
+++ proc.c
@@ -199,7 +199,7 @@ fork(void)
   np->sz = curproc->sz;
   np->parent = curproc;
   *np->tf = *curproc->tf;
-  np->kstack = curproc->kstack;					//ADDED LAB3: copys loc of kernal stack
+  //np->kstack = curproc->kstack;					//ADDED LAB3: copys loc of kernal stack
 
   // Clear %eax so that fork returns 0 in the child.
   np->tf->eax = 0;

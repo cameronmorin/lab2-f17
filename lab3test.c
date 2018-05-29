c720a21aa47010b948a184585e3c1c12ebab9ed1
cameronmorin <cmori007@ucr.edu>
2018-05-28 20:22:57 -0700

    Changed test case

--- lab3test.c
+++ lab3test.c
@@ -1,34 +1,57 @@
+// #include "types.h"
+// #include "stat.h"
+// #include "user.h"
+
+// uint fib(uint n) {
+//   if (n == 0 || n == 1) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// } 
+
+// int returnzero(int n) {
+//   int data[10000];
+//   data[0] = 0;
+//   if (n == 0) {
+//     return 0;
+//   }
+
+//   printf(1, "%d\n", n);
+//   return returnzero(--n);
+// }
+
+// int main(int argc, char const *argv[]) {
+//   // printf(1, "fib(10) = %d\n", fib(10));
+//   // printf(1, "fib(20) = %d\n", fib(20));
+//   // printf(1, "fib(30) = %d\n", fib(30));
+//   // printf(1, "fib(40) = %d\n", fib(40));
+
+//   returnZero(100);
+
+//   exit();
+//   return 0;
+// }
+
+
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 
-uint fib(uint n) {
-  if (n == 0 || n == 1) {
-    return n;
-  }
-
-  return fib(n - 1) + fib(n - 2);
-} 
-
-int returnzero(int n) {
-  int data[10000];
-  data[0] = 0;
-  if (n == 0) {
-    return 0;
-  }
-
-  printf(1, "%d\n", n);
-  return returnzero(--n);
+int test(int n)
+{ 
+   int x = n + 1;
+   return x;
 }
-
-int main(int argc, char const *argv[]) {
-  // printf(1, "fib(10) = %d\n", fib(10));
-  // printf(1, "fib(20) = %d\n", fib(20));
-  // printf(1, "fib(30) = %d\n", fib(30));
-  // printf(1, "fib(40) = %d\n", fib(40));
-
-  returnZero(100);
-
-  exit();
-  return 0;
+int main(int argc, char *argv[])
+{
+   int pid=0;
+   pid=fork();
+   if(pid==0){
+       test(1);
+       exit();
+   }
+   wait();
+   exit();
 }

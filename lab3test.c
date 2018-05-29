a24465251fdf953b5118a18541812dc6b349f0d5
Cameron <cmori007@ucr.edu>
2018-05-29 00:05:08 -0700

    Test changes

--- lab3test.c
+++ lab3test.c
@@ -1,44 +1,48 @@
-// #include "types.h"
-// #include "stat.h"
-// #include "user.h"
+ #include "types.h"
+ #include "stat.h"
+ #include "user.h"
 
-// uint fib(uint n) {
-//   if (n == 0 || n == 1) {
-//     return n;
-//   }
+ uint fib(uint n) {
+    if (n == 0 || n == 1) {
+        return n;
+    }
 
-//   return fib(n - 1) + fib(n - 2);
-// } 
+    if (n == 50 || n == 60 || n == 70 || n == 80 || n == 90) {
+        printf(1, "%d\n", n);
+    }
+    return fib(n - 1) + fib(n - 2);
+ } 
 
-// int main(int argc, char const *argv[]) {
-//   // printf(1, "fib(10) = %d\n", fib(10));
-//   // printf(1, "fib(20) = %d\n", fib(20));
-//   // printf(1, "fib(30) = %d\n", fib(30));
-//   // printf(1, "fib(40) = %d\n", fib(40));
+ int main(int argc, char const *argv[]) {
+    printf(1, "fib(10) = %d\n", fib(10));
+    printf(1, "fib(20) = %d\n", fib(20));
+    printf(1, "fib(30) = %d\n", fib(30));
+    printf(1, "fib(40) = %d\n", fib(40));
+    printf(1, "fib(90) = %d\n", fib(90));	
 
-//   exit();
-//   return 0;
-// }
-
-
-
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-
-int test(int n)
-{ 
-   int x = n + 1;
-   return x;
-}
-int main(int argc, char *argv[])
-{
-   int pid=0;
-   pid=fork();
-   if(pid==0){
-       test(1);
-       exit();
-   }
-   wait();
    exit();
-}
+   return 0;
+ }
+
+
+
+//#include "types.h"
+//#include "stat.h"
+//#include "user.h"
+
+//int test(int n)
+//{ 
+//   int x = n + 1;
+//   return x;
+//}
+//int main(int argc, char *argv[])
+//{
+//   int pid=0;
+//   pid=fork();
+//   if(pid==0){
+//       test(1);
+//       exit();
+//   }
+//  wait();
+//   exit();
+//}

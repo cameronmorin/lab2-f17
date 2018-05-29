a198701ef35c4542ae9a158b3fb672424e352d2a
cameronmorin <cmori007@ucr.edu>
2018-05-28 20:26:47 -0700

    Test cases

--- lab3test.c
+++ lab3test.c
@@ -10,24 +10,11 @@
 //   return fib(n - 1) + fib(n - 2);
 // } 
 
-// int returnzero(int n) {
-//   int data[10000];
-//   data[0] = 0;
-//   if (n == 0) {
-//     return 0;
-//   }
-
-//   printf(1, "%d\n", n);
-//   return returnzero(--n);
-// }
-
 // int main(int argc, char const *argv[]) {
 //   // printf(1, "fib(10) = %d\n", fib(10));
 //   // printf(1, "fib(20) = %d\n", fib(20));
 //   // printf(1, "fib(30) = %d\n", fib(30));
 //   // printf(1, "fib(40) = %d\n", fib(40));
-
-//   returnZero(100);
 
 //   exit();
 //   return 0;
